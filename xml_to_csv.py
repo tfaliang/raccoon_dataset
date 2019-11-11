c88d20caf389f367128276e9fba7e3544ca58027
tfaliang <57636082+tfaliang@users.noreply.github.com>
2019-11-12 01:19:12 +0800

    Update xml_to_csv.py

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -26,9 +26,10 @@ def xml_to_csv(path):
 
 
 def main():
-    image_path = os.path.join(os.getcwd(), 'annotations')
+     for directory in ['train', 'test']:
+    image_path = os.path.join(os.getcwd(), 'images/{}'.format(directory))
     xml_df = xml_to_csv(image_path)
-    xml_df.to_csv('raccoon_labels.csv', index=None)
+    xml_df.to_csv('data/{}_labels.csv'.format(directory), index=None)
     print('Successfully converted xml to csv.')
 
 
